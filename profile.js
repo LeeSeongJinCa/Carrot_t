0be4ba07e9df2367bda2ece35ba63c3266e9b41e
이성진 <chlskatnr1@naver.com>
2019-07-16 19:36:04 +0900

    ADD profile.js
    제출 및 수정, 추가 버튼, 기능 추가

--- profile.js
+++ profile.js
@@ -1,36 +1,51 @@
 window.onload = () => {
-    var p = document.getElementsByClassName('item-info');
-    var img = document.getElementsByClassName('profile-img');
-    // var img = document.querySelector('.profile-img');
-    console.log(p);
-    console.log(img);
-    for (var i = 0; i < p.length; i++) {
-        if (p[i].innerHTML != "") {
-            img[i].style.backgroundColor = "#737373";
+    document.getElementById('item-button').addEventListener("click", (count) => {
+        const gridItem = document.getElementsByClassName('grid-item');
+        const texts = document.getElementsByClassName('item-input');
+        const img = document.getElementsByClassName('profile-img');
+        const p = document.getElementsByClassName('item-info');
+        const button = document.getElementById('item-button');
+        var text = {};
+        for (var i = 0; i < gridItem.length; i++) {
+            if (texts[i].value != "") {
+                text = texts[i].value;
+                p[i].innerHTML = text;
+                img[i].style.backgroundColor = "#737373";
+            }
+            else {
+                p[i].innerHTML = "";
+                img[i].style.backgroundColor = "#C4C4C4";
+            }
         }
-    }
-}
+        for (var i = 0; i < p.length; i++) {
+            if (p[i].innerHTML !== "") {
+                count = 1;
+            }
+        }
+        if (count == 1) {
+            button.innerHTML = "수정";
+            count = 0;
+        }
+        else { button.innerHTML = "제출"; }
+    });
 
-function a() {
-    var gridItem = document.getElementsByClassName('grid-item');
-    var texts = document.getElementsByClassName('item-input');
-    var img = document.getElementsByClassName('profile-img');
-    var p = document.getElementsByClassName('item-info');
-    var text = {};
-    for(var i = 0; i < texts.length; i++) {
-        if (texts[i].value != "") {
-            // console.log(texts[i].value);
-            text = texts[i].value;
-            p[i].innerHTML = text;
-            texts[i].remove();
-            img[i].style.backgroundColor = "#737373";
-        }
-        else {
-            img[i].style.backgroundColor = "#C4C4C4";
-        }
-    }
+    document.getElementById("plus-button").addEventListener("click", () => {
+        var gridContainer = document.getElementsByClassName('grid-container');
+        var gridItem = document.createElement('div');
+        var profileImg = document.createElement('div');
+        var itemInfo = document.createElement('p');
+        var itemInput = document.createElement('input');
+        gridItem.setAttribute('class', 'grid-item');
+        profileImg.setAttribute('class', 'profile-img');
+        itemInfo.setAttribute('class', 'item-info');
+        itemInput.setAttribute('type', 'text');
+        itemInput.setAttribute('class', 'item-input');
+        gridItem.appendChild(profileImg);
+        gridItem.appendChild(itemInfo);
+        gridItem.appendChild(itemInput);
+        gridContainer[0].appendChild(gridItem);
+    });
 }
-
 
 
 
