153ed0be65f0ae89717d61d8c13f65db1834dce9
이성진 <chlskatnr1@naver.com>
2019-07-16 23:30:33 +0900

    ADD profile.js

--- profile.js
+++ profile.js
@@ -1,3 +1,4 @@
+var serverUrl = "http://api.teamrequin.kro.kr";
 window.onload = () => {
     document.getElementById('item-button').addEventListener("click", (count) => {
         const gridItem = document.getElementsByClassName('grid-item');
@@ -6,27 +7,36 @@ window.onload = () => {
         const p = document.getElementsByClassName('item-info');
         const button = document.getElementById('item-button');
         var text = {};
-        for (var i = 0; i < gridItem.length; i++) {
-            if (texts[i].value != "") {
-                text = texts[i].value;
-                p[i].innerHTML = text;
-                img[i].style.backgroundColor = "#737373";
+        var data = {
+            "search_id": texts[0].value
+        };
+        axios.post(serverUrl + "/service/search-user", data).then(() => {
+            for (var i = 0; i < gridItem.length; i++) {
+                if (texts[i].value != "") {
+                    text = texts[i].value;
+                    p[i].innerHTML = text;
+                    img[i].style.backgroundColor = "#737373";
+                }
+                else {
+                    p[i].innerHTML = "";
+                    img[i].style.backgroundColor = "#C4C4C4";
+                }
             }
-            else {
-                p[i].innerHTML = "";
-                img[i].style.backgroundColor = "#C4C4C4";
+            for (var i = 0; i < p.length; i++) {
+                if (p[i].innerHTML !== "") {
+                    count = 1;
+                }
             }
-        }
-        for (var i = 0; i < p.length; i++) {
-            if (p[i].innerHTML !== "") {
-                count = 1;
+            // axios.post('')
+            if (count == 1) {
+                button.innerHTML = "수정";
+                count = 0;
             }
-        }
-        if (count == 1) {
-            button.innerHTML = "수정";
-            count = 0;
-        }
-        else { button.innerHTML = "제출"; }
+            else { button.innerHTML = "제출"; }
+            alert('성공');
+        }).catch(() => {
+            alert('실패');
+        });
     });
 
     document.getElementById("plus-button").addEventListener("click", () => {
@@ -45,9 +55,7 @@ window.onload = () => {
         gridItem.appendChild(itemInput);
         gridContainer[0].appendChild(gridItem);
     });
-}
-
-
+};
 
 
 
